045fd5e4b3c748251c09f272c04df03397dc1000
Wawta1234 <nan.wawta@gmail.com>
2024-02-12 16:16:52 +0700

    add logout system

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,13 +1,22 @@
 // Header.js
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Header.css"; // คุณสามารถสร้างไฟล์ CSS นี้เพื่อใช้ในการจัดรูปแบบ
 
 const Header = () => {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    navigate("/", { replace: true });
+  };
+  
   return (
     <header className="header">
       <h1>TULP<br /> 
         Booking Room</h1> {/* ชื่อ "TULP Booking Room" อยู่ทางซ้ายมือ */}
-      <i className="bi bi-person"></i> {/* ไอคอนคนอยู่ขวามือ */}
+         <a onClick={handleLogout}>ออกจากระบบ</a> 
+      {/* <i className="bi bi-person"onClick={handleLogout}></i> ไอคอนคนอยู่ขวามือ */}
     </header>
   );
 };
